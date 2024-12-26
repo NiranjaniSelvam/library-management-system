4ded73b6b566c2fe1defd79d9961a8831fca4298
NiranjaniSelvam <149168244+NiranjaniSelvam@users.noreply.github.com>
2024-12-26 20:58:06 +0530

    Delete tests/tests directory

--- tests/tests/test_library.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from library_management.library import Library
-
-def test_add_book():
-    library = Library()
-    library.add_book(isbn="12345", title="Python Basics", author="John Doe", year=2023)
-    books = library.view_available_books()
-    assert len(books) == 1
-    assert books[0]['isbn'] == "12345"
